8796375f08d4be23e8aa7feab49a2bcc886596c0
dianavfreitas <dianavfreitass@gmail.com>
2024-04-25 20:15:34 -0400

    pushing updated home page

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ function showSideBar(){
     const sideBar = document.querySelector('.sidebar')
     sideBar.style.display = 'flex'
 }
-
 function hideSideBar(){
     const sideBar = document.querySelector('.sidebar')
     sideBar.style.display = 'none'
 }
-
